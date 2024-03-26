42cba308df12504a1603be13799cb151b0d4348b
Shubin Hu <SeekAWayOut@gmail.com>
2024-03-27 00:17:13 +0800

    fix some bug

--- BGSQml.cmake
+++ BGSQml.cmake
@@ -1,10 +1,15 @@
-include(${CMAKE_SOURCE_DIR}/cmake/BGStudio.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/BGStudio.cmake)
 
 if(ANDROID)
-    set(platform "ANDROID")
+  set(platform "ANDROID")
 else()
-    set(platform "desktop")
+  set(platform "desktop")
 endif()
 
-set(BGS_QML_IMPORT_PATH "${CMAKE_INSTALL_PREFIX}/qml/${platform}" CACHE STRING "")
-set(QML_IMPORT_PATH ${BGS_QML_IMPORT_PATH} CACHE STRING "")
+set(BGS_QML_IMPORT_PATH
+    "${CMAKE_INSTALL_PREFIX}/qml/${platform}"
+    CACHE STRING "")
+set(QML_IMPORT_PATH
+    ${BGS_QML_IMPORT_PATH}
+    CACHE STRING "")
+add_definitions(-DBGS_QML_IMPORT_PATH="${BGS_QML_IMPORT_PATH}")
